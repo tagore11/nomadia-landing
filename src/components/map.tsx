da5225383215cdb8913251f6a7d1d7169c9c8a08
“tagore11 <said.mehmet@gmail.comgit config --global user.name tagore11git config --global user.email said.mehmet@gmail.com>
2025-05-14 01:17:15 +0300

    Map görünürlük testi

--- src/components/map.tsx
+++ src/components/map.tsx
@@ -1,37 +1,19 @@
 "use client";
 
-import { GoogleMap, Marker, useLoadScript } from "@react-google-maps/api";
-import { useMemo } from "react";
-
-const containerStyle = {
-  width: "100%",
-  height: "400px",
-};
-
-const center = {
-  lat: 36.2012, // Kaş
-  lng: 29.6371,
-};
-
-const MapComponent = () => {
-  const { isLoaded, loadError } = useLoadScript({
-    googleMapsApiKey: "AIzaSyDYyuvzqrXx07kSKDZj5x1UtUx3-6fXlbs",
-  });
-
-  const mapCenter = useMemo(() => center, []);
-
-  if (loadError) return <div>Error loading map</div>;
-  if (!isLoaded) return <div>Loading map...</div>;
-
+export default function Map() {
   return (
-    <GoogleMap
-      mapContainerStyle={containerStyle}
-      center={mapCenter}
-      zoom={13}
+    <div
+      style={{
+        backgroundColor: "lightblue",
+        width: "100%",
+        height: "100%",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        fontWeight: "bold",
+      }}
     >
-      <Marker position={mapCenter} />
-    </GoogleMap>
+      Harita burada
+    </div>
   );
-};
-
-export default MapComponent;
+}
